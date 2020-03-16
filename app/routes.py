8be1b2471725f5acee4ad19b4619e707b2b4760a
Dean Vains <dean.vains@outlook.com>
2020-03-16 13:26:04 +0800

    Main work done, now minor tweaks

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,54 @@
+import sys
+import logging
+import os
 from app import app
 from flask import render_template, request, Response, jsonify
 from app.discord import sendDetailsToDiscord
 from flask_restful import Resource, Api
-import sys
+import requests
+
 api = Api(app)
+
+GA_TRACKING_ID="UA-160764698-1"
+
+def track_event(category, action, label=None, value=0):
+    data = {
+        'v': '1',  # API Version.
+        'tid': GA_TRACKING_ID,  # Tracking ID / Property ID.
+        # Anonymous Client Identifier. Ideally, this should be a UUID that
+        # is associated with particular user, device, or browser instance.
+        'cid': '555',
+        't': 'event',  # Event hit type.
+        'ec': category,  # Event category.
+        'ea': action,  # Event action.
+        'el': label,  # Event label.
+        'ev': value,  # Event value, must be an integer
+        'ua': 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14'
+    }
+
+    response = requests.post(
+        'https://www.google-analytics.com/collect', data=data)
+
+    # If the request fails, this will raise a RequestException. Depending
+    # on your application's needs, this may be a non-error and can be caught
+    # by the caller.
+    response.raise_for_status()
+
+
 
 @app.route('/',methods=["GET"])
 @app.route('/',methods=["GET"])
 def index():
+    track_event(
+        category='Example',
+        action='test action')
     return render_template("/www/index.html")
 
 @app.route('/submitted',methods=["GET", "POST"])
 def submitted():
+    track_event(
+        category='Example',
+        action='test action')
     if request.method == 'POST':
         sendDetailsToDiscord(request.form['name'],request.form['email'],request.form['phone'],request.form['state'],request.form['course'],request.environ['REMOTE_ADDR'],request.headers.get('User-Agent')) #need to ad request ip and request web client
         
@@ -57,3 +94,11 @@ class Form(Resource):
 
 
 api.add_resource(Form, '/api')
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
